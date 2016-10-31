a2f286daec68625546eeefaa65cdb8fb2e9dc2f7
Maria Patterson <mtpatter@uw.edu>
2016-10-31 15:42:03 -0700

    Reconfigure alert producer to encode each alert separately.
    Previous alert producer encoded one template alert and sent to Kafka repeatedly.

--- bin/sendAlertStream.py
+++ bin/sendAlertStream.py
@@ -4,46 +4,49 @@
 """
 
 from __future__ import print_function
+import time
 import argparse
 import json
-from confluent_kafka import Producer
-from lsst.alert.stream import avroAlertGen
-from lsst.alert.stream import avroUtils
+from lsst.alert.stream import alertProducer
 
 
 def main():
     parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument('topic', metavar='topic', type=str,
+    parser.add_argument('topic', type=str,
                         help='Name of Kafka topic stream to push to.')
-    parser.add_argument('ccd', metavar='ccdId', type=int,
-                        help='CCD ID number.')
-    parser.add_argument('alertnum', metavar='alertnum', type=int,
-                        help='Number of alerts per CCD.')
+    parser.add_argument('alertnum', type=int,
+                        help='Number of alerts to send.')
 
     args = parser.parse_args()
-    topic = args.topic
-    ccdnum = args.ccd
-    alertnum = args.alertnum
 
-    # Set up producer connection to Kafka broker
+    # Configure producer connection to Kafka broker
     conf = {'bootstrap.servers': 'localhost:9092'}
-    producer = Producer(**conf)
 
     # Configure Avro writer schema and data
-    schema_path = ["../sample-avro-alert/schema/diasource.avsc",
-                   "../sample-avro-alert/schema/diaobject.avsc",
-                   "../sample-avro-alert/schema/ssobject.avsc",
-                   "../sample-avro-alert/schema/alert.avsc"]
-    alert_schema = avroUtils.combineSchemas(schema_path)
+    schema_files = ["../sample-avro-alert/schema/diasource.avsc",
+                    "../sample-avro-alert/schema/diaobject.avsc",
+                    "../sample-avro-alert/schema/ssobject.avsc",
+                    "../sample-avro-alert/schema/alert.avsc"]
     json_path = "../sample-avro-alert/data/alert.json"
 
     # Load template alert contents
     with open(json_path) as file_text:
         json_data = json.load(file_text)
 
-    # Generate and send alerts to Kafka producer
-    alertGen = avroAlertGen.AvroAlertGen(json_data, alert_schema, ccdnum)
-    alertGen.sendAlerts(producer, topic, alertnum)
+    # Configure Kafka producer with topic and schema
+    streamProducer = alertProducer.AlertProducer(args.topic, schema_files, **conf)
+
+    start_time = time.time()
+    print("start time:{:.3f}".format(start_time))
+
+    # Send alerts to producer
+    for i in range(args.alertnum):
+        streamProducer.send(json_data, encode=True)
+    streamProducer.flush()
+    finish_time = time.time()
+
+    print("finish time:{:.3f}".format(finish_time))
+    print("delta time:{:.3f}".format(finish_time - start_time))
 
 
 if __name__ == "__main__":
---
+++ python/lsst/alert/stream/alertProducer.py
@@ -0,0 +1,45 @@
+from __future__ import print_function
+import confluent_kafka
+from . import avroUtils
+
+__all__ = ['AlertProducer']
+
+
+class AlertProducer(object):
+    """Alert stream producer with Kafka.
+
+    Parameters
+    ----------
+    topic : `str`
+        The name of the topic stream for writing.
+    schema : Avro schema
+        The writer Avro schema for encoding data. Optional.
+    **kwargs
+        Keyword arguments for configuring confluent_kafka.Producer().
+    """
+
+    def __init__(self, topic, schema_files=None, **kwargs):
+        self.producer = confluent_kafka.Producer(**kwargs)
+        self.topic = topic
+        if schema_files is not None:
+            self.alert_schema = avroUtils.combineSchemas(schema_files)
+
+    def send(self, data, encode=False):
+        """Sends a message to Kafka stream.
+
+        Parameters
+        ----------
+        data : message content
+            Data containing message content.  If encode is True, expects JSON format.
+        encode : `boolean`
+            If True, encodes data to Avro format. If False, sends data raw.
+        """
+        if encode is True:
+            avro_bytes = avroUtils.writeAvroData(data, self.alert_schema)
+            raw_bytes = avro_bytes.getvalue()
+            self.producer.produce(self.topic, raw_bytes)
+        else:
+            self.producer.produce(self.topic, str(data))
+
+    def flush(self):
+        return self.producer.flush()
