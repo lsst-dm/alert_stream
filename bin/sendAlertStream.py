b28de93376eb2fda9ebde54a2fbbb769c6319dfa
Maria Patterson <mtpatter@uw.edu>
2017-01-27 16:37:19 -0800

    Extend alert sender to produce a continuous stream
    Before this commit, sendAlertStream.py sends a burst of alerts
    and stops. Adding ability here to start a continous stream of
    alerts on the 39th second, so as to be able to scale up
    synchronized streams corresponding to each CCD.

--- bin/sendAlertStream.py
+++ bin/sendAlertStream.py
@@ -8,6 +8,7 @@ import time
 import argparse
 import json
 import os.path
+import asyncio
 from lsst.alert.stream import alertProducer
 
 
@@ -19,6 +20,29 @@ def load_stamp(file_path):
         cutout_data = f.read()
         cutout_dict = {"fileName": fileoutname, "stampData": cutout_data}
     return cutout_dict
+
+
+@asyncio.coroutine
+def delay(wait_sec, function):
+    """Sleep for a given time before calling a function.
+    """
+    print('delay starting: {}'.format(time.time()))
+    yield from asyncio.sleep(wait_sec)
+    print('delay done sleeping: {}'.format(time.time()))
+    return function()
+    print('delay finished: {}'.format(time.time()))
+
+
+@asyncio.coroutine
+def schedule_delays(eventloop, function, maxcounts):
+    counter = 0
+    while counter < maxcounts:
+        seconds_to_minute = 39 - (time.time() % 39)  # on the 39th second
+        yield from asyncio.ensure_future(delay(seconds_to_minute, function))
+        counter += 1
+        print('batches finished: {}'.format(counter))
+    else:
+        eventloop.stop()
 
 
 def main():
@@ -37,7 +61,12 @@ def main():
                            help='Encode to Avro format. (default)')
     avrogroup.add_argument('--encode-off', dest='avroFlag', action='store_false',
                            help='Do not encode to Avro format.')
-    parser.set_defaults(stamps=True, avroFlag=True)
+    parser.add_argument('--repeat', action='store_true',
+                        help='Send alert batches repeating every 39th second.'
+                        ' Default of 2215 batches (~24 hours).')
+    parser.add_argument('--max-repeats', type=int, dest='batchnum',
+                        help='Override default number of batches to send.')
+    parser.set_defaults(stamps=True, avroFlag=True, batchnum=2215)
 
     args = parser.parse_args()
 
@@ -66,17 +95,26 @@ def main():
     # Configure Kafka producer with topic and schema
     streamProducer = alertProducer.AlertProducer(args.topic, schema_files, **conf)
 
-    start_time = time.time()
-    print("start time:{:.3f}".format(start_time))
+    def send_batch():
+        start_time = time.time()
+        print('batch start time:{:.3f}'.format(start_time))
+        for i in range(args.alertnum):
+            streamProducer.send(json_data, encode=args.avroFlag)
+        streamProducer.flush()
+        finish_time = time.time()
+        print('batch finish time:{:.3f}'.format(finish_time))
+        print('batch delta time:{:.3f}'.format(finish_time - start_time))
 
     # Send alerts to producer
-    for i in range(args.alertnum):
-        streamProducer.send(json_data, encode=args.avroFlag)
-    streamProducer.flush()
-    finish_time = time.time()
-
-    print("finish time:{:.3f}".format(finish_time))
-    print("delta time:{:.3f}".format(finish_time - start_time))
+    print('start: {}'.format(time.time()))
+    if args.repeat:
+        loop = asyncio.get_event_loop()
+        asyncio.ensure_future(schedule_delays(loop, send_batch, args.batchnum))
+        loop.run_forever()
+        loop.close()
+    else:
+        send_batch()
+    print('finish: {}'.format(time.time()))
 
 
 if __name__ == "__main__":
