c12dcc4e4d56cb5192242d00def9f4e9f316c5fd
Maria Patterson <mtpatter@uw.edu>
2018-05-04 10:03:11 -0700

    Make sender produce every 39 seconds.

--- bin/sendAlertStream.py
+++ bin/sendAlertStream.py
@@ -8,7 +8,45 @@ from __future__ import print_function
 import argparse
 import glob
 import time
+import asyncio
 from lsst.alert.stream import alertProducer, avroUtils
+
+
+@asyncio.coroutine
+def delay(wait_sec, function, *args):
+    """Sleep for a given time before calling a function.
+    Parameters
+    ----------
+    wait_sec
+        Time in seconds to sleep before calling `function`.
+    function
+        Function to return after sleeping.
+    """
+    yield from asyncio.sleep(wait_sec)
+    return function(*args)
+
+
+@asyncio.coroutine
+def schedule_delays(eventloop, function, argslist, interval=39):
+    """Schedule delayed calls of functions at a repeating interval.
+    Parameters
+    ----------
+    eventloop
+        Event loop returned by asyncio.get_event_loop().
+    function
+        Function to be scheduled.
+    argslist
+        List of inputs for function to loop over.
+    interval
+        Time in seconds between calls.
+    """
+    counter = 1
+    for arg in argslist:
+        wait_time = interval - (time.time() % interval)
+        yield from asyncio.ensure_future(delay(wait_time, function, arg))
+        print('visits finished: {} \t time: {}'.format(counter, time.time()))
+        counter += 1
+    eventloop.stop()
 
 
 def main():
@@ -32,7 +70,9 @@ def main():
     # Scan for avro files
     root = "../data"
     files = [f for f in glob.glob("/".join([root, "*.avro"]))]
-    for f in files:
+    files.sort()
+
+    def send_visit(f):
         print('visit:', f[15:20], '\ttime:', time.time())
         # Load alert contents
         with open(f, mode='rb') as file_data:
@@ -46,7 +86,12 @@ def main():
                     alert_count += 1
                 else:
                     break
-            streamProducer.flush()
+        streamProducer.flush()
+
+    loop = asyncio.get_event_loop()
+    asyncio.ensure_future(schedule_delays(loop, send_visit, files))
+    loop.run_forever()
+    loop.close()
 
 
 if __name__ == "__main__":
