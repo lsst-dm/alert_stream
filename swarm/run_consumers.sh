de7279709c5986143bcbe6013eb962d139e78db1
Maria Patterson <mtpatter@uw.edu>
2018-09-12 14:37:06 -0700

    Add AWS instructions.

--- swarm/run_consumers.sh
+++ swarm/run_consumers.sh
@@ -14,7 +14,7 @@ for i in `seq  $3 $4`
         --name monitor$filnum \
         --network alert_stream_default \
         --constraint node.id==$1 \
-	--endpoint-mode=dnsrr \
+        --endpoint-mode=dnsrr \
         -e PYTHONUNBUFFERED=0 \
         alert_stream python bin/monitorStream.py $2 Filter$filnum
     done
--- swarm/run_sender.sh
+++ swarm/run_sender.sh
@@ -6,6 +6,6 @@ docker service create \
               --name sender \
               --network alert_stream_default \
               --constraint node.id==$1 \
-	      --endpoint-mode=dnsrr \
+              --endpoint-mode=dnsrr \
               -e PYTHONUNBUFFERED=0 \
               alert_stream python bin/sendAlertStream.py kafka:9092 full-stream
